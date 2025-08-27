f5426374764c359f7c6927de197893ec84ba07b1
OwenSugden <owen.sugden04@gmail.com>
2025-08-27 17:40:15 +1200

    finished up the recipe_detail page and edited some things for browse

--- recipe/recipe_detail/recipe_detail.py
+++ recipe/recipe_detail/recipe_detail.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, url_for
 import recipe.recipe_detail.services as services
 import recipe.adapters.repository as repo
 
@@ -8,7 +8,10 @@ recipe_detail_blueprint = Blueprint('recipe_detail_bp', __name__)
 @recipe_detail_blueprint.route('/browse/<int:recipe_id>', methods=['GET'])
 def recipe_detail(recipe_id):
     recipe_object = services.get_recipe(repo.repo_instance, recipe_id)
-    return render_template('recipe_detail.html', recipe=recipe_object)
+    return_to = request.args.get("return_to", url_for("browse_bp.browse"))
+
+    return render_template('recipe_detail.html', recipe=recipe_object,
+                           return_to=return_to)
 
 
 
