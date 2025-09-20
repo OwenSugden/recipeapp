f06cd510dd4578ecf77d564a3dc4dc1e97278e77
OwenSugden <owen.sugden04@gmail.com>
2025-09-21 02:52:19 +1200

    fixed bug

--- recipe/browse/services.py
+++ recipe/browse/services.py
@@ -12,7 +12,13 @@ def get_recipe(repo: AbstractRepository):
             'id': recipe.id,
             'name': recipe.name,
             'author': recipe.author.name,
-            'images': recipe.images
+            'images': recipe.images,
+            'category': recipe.category.name,
+            'time': recipe.cook_time + recipe.preparation_time,
+            'calories': recipe.nutrition.calories,
+            'protein': recipe.nutrition.protein,
+            'fat': recipe.nutrition.fat,
+            'carbohydrates': recipe.nutrition.carbohydrates
         }
         recipe_dicts.append(recipe_dict)
     return recipe_dicts
