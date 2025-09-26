99c4effb4ba88a2bd978d85f5626d4d5a863ffd5
Ben <bcha428@aucklanduni.ac.nz>
2025-09-26 21:01:39 +1200

    @login_required proper implementation

--- recipe/comments/comments.py
+++ recipe/comments/comments.py
@@ -1,16 +1,17 @@
 from flask import Blueprint, request, redirect, url_for, session
 import recipe.adapters.repository as repo
 import recipe.comments.services as services
+from recipe.authentication.authentication import login_required
 
 comments_blueprint = Blueprint('comments_bp', __name__)
 
 
 @comments_blueprint.route('/browse/<int:recipe_id>/comment', methods=['POST'])
+@login_required
 def add_comment(recipe_id):
     # Redirect to login if not logged in
-    if 'user_name' not in session or not session['user_name'] or session is None:
-        print("HELLO")
-        return redirect(url_for('authentication_bp.login', next=request.referrer or '/'))
+    # if 'user_name' not in session or not session['user_name'] or session is None:
+    #     return redirect(url_for('authentication_bp.login', next=request.referrer or '/'))
 
     user_name = session['user_name']
     comment_text = request.form.get('comment')
--- recipe/ratings/ratings.py
+++ recipe/ratings/ratings.py
@@ -1,18 +1,15 @@
 from flask import Blueprint, request, redirect, url_for, session
 import recipe.adapters.repository as repo
 import recipe.ratings.services as services
+from recipe.authentication.authentication import login_required
 
 ratings_blueprint = Blueprint('ratings_bp', __name__)
 
 @ratings_blueprint.route('/rate/<int:recipe_id>', methods=['POST'])
+@login_required
 def rate_recipe(recipe_id):
-    if 'user_name' not in session or not session['user_name'] or session is None:
-        return redirect(url_for('authentication_bp.login', next=request.path))
-
     user_name = session['user_name']
     value = request.form.get('rating')
-
-
 
     if value:
         services.add_rating(repo.repo_instance, recipe_id, user_name, int(value))
