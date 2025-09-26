df378449044dace99e613a105943edb2e0981e1b
Ben <bcha428@aucklanduni.ac.nz>
2025-09-26 20:57:19 +1200

    Integrate rating feature into recipe detail page

--- recipe/recipe_detail/recipe_detail.py
+++ recipe/recipe_detail/recipe_detail.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request
 import recipe.adapters.repository as repo
 import recipe.recipe_detail.services as services
+import recipe.ratings.services as ratings
+from recipe.ratings.services import get_average_rating
 
 recipe_detail_blueprint = Blueprint('recipe_detail_bp', __name__)
 
@@ -9,10 +11,13 @@ def recipe_detail(recipe_id):
     return_to = request.args.get("return_to")
     recipe_object = services.get_recipe(repo.repo_instance, recipe_id)
     comments = services.get_comments(repo.repo_instance, recipe_id)
+    avg_rating = ratings.get_average_rating(repo.repo_instance, recipe_id)
     return render_template('recipe_detail.html',
                            recipe=recipe_object,
                            comments=comments,
-                           return_to=return_to)
+                           avg_rating=avg_rating,
+                           return_to=return_to
+                           )
 
 
 
